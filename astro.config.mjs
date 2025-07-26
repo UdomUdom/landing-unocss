b28882e0dc19a4504fc4f80df0ea7f424fcc8bb6
UdomUdom <udomzaark@gmail.com>
2025-07-27 01:15:15 +0700

    update unocss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import unocss from '@unocss/astro';
 import astroI18next from "astro-i18next";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   integrations: [unocss(), astroI18next()],
   vite: {
-    plugins: [tailwindcss()],
   }, i18n: {
     defaultLocale: "th",
     locales: ["th", "en"],
