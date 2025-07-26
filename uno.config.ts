d865eaf17c26d3a22483d88fc2b28855b30c3c8f
Sunny .dev <teeseeker1015@gmail.com>
2025-07-26 17:36:18 +0700

    Add search box component and update layout; include new images and dependencies

--- uno.config.ts
+++ uno.config.ts
@@ -1,9 +1,12 @@
-// uno.config.ts
 import { defineConfig, presetUno, presetIcons } from 'unocss'
 
 export default defineConfig({
+
   presets: [
     presetUno(),
-    presetIcons(), 
+    presetIcons({
+      scale: 1,  
+      warn: true, 
+    }),
   ],
 })
