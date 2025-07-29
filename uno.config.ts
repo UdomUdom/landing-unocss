ea2354260ff3412f80585bc5e1f87a29d5f43990
UdomUdom <udomzaark@gmail.com>
2025-07-30 04:47:49 +0700

    update page

--- uno.config.ts
+++ uno.config.ts
@@ -8,4 +8,8 @@ export default defineConfig({
       warn: true,
     }),
   ],
+  shortcuts: {
+    "btn-primary":
+      "bg-blue hover:bg-blue-500 border-0 transition-colors duration-200 px-6 py-2 shadow-md",
+  },
 });
